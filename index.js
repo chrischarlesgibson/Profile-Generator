1a96b4df7558ea6ee935e2fe740a04bb04edea52
Chris Gibson <chrischarlesgibson@gmail.com>
2022-10-23 17:34:24 -0500

    added index.js with questions arrays

--- index.js
+++ index.js
@@ -2,8 +2,82 @@ const inquirer = require("inquier");
 const fs = require("fs");
 const jest = require("jest");
 
-// const renderHTML = (answers) =>
+//question array to direct the user to the correct set of employee questions array
+const typeOfEmployeeQuestion = [
+  {
+    type: "list",
+    message: "Which type of team member do you want to add?",
+    name: "teamMemberType",
+    choices: ["manager", "Egineer", "Intern"],
+  },
+];
 
-const employeeQuestions = [{
-    type:
-}]
+// question array to be asked if users selects that they what to input manager info
+const ManagerQuestions = [
+  {
+    type: "input",
+    message: "Enter the mananger's full name",
+    name: "managerName",
+  },
+  {
+    type: "input",
+    message: "Enter the mananger's ID",
+    name: "managerId",
+  },
+  {
+    type: "input",
+    message: "Enter the mananger's email address",
+    name: "managerEmail",
+  },
+  {
+    type: "input",
+    message: "Enter the mananger's office number",
+    name: "managerOffice",
+  },
+];
+// question array to be asked if users selects that they what to input intern info
+const internQuestions = [
+  {
+    type: "input",
+    message: "Enter the intern's full name",
+    name: "internName",
+  },
+  {
+    type: "input",
+    message: "Enter the intern's ID",
+    name: "internId",
+  },
+  {
+    type: "input",
+    message: "Enter the intern's email address",
+    name: "internEmail",
+  },
+  {
+    type: "input",
+    message: "Enter the intern's school",
+    name: "internSchool",
+  },
+];
+// question array to be asked if users selects that they what to input engineer info
+const engineerQuestions = [
+  {
+    type: "input",
+    message: "Enter the engineer's full name",
+    name: "engineerName",
+  },
+  {
+    type: "input",
+    message: "Enter the engineer's ID",
+    name: "engineerId",
+  },
+  {
+    type: "input",
+    message: "Enter the engineer's email address",
+    name: "engineerEmail",
+  },
+  {
+    type: "input",
+    message: "Enter the engineer's Github username",
+    name: "engineerGithub",
+  },
+];
