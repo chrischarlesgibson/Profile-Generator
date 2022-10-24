f571936e8093d91664a2d7bdd4635165a6d03e22
Chris Gibson <chrischarlesgibson@gmail.com>
2022-10-23 22:31:37 -0500

    got 4 tests to pass

--- classes/jest tests/Engineer.test.js
+++ classes/jest tests/Engineer.test.js
@@ -2,8 +2,13 @@ const Engineer = require("../Engineer");
 
 //test to check if the employee object contains the necessary info like id, name and email
 describe("Engineer", function () {
-  it("should contain the engineers's name, id , emailand github ", function () {
-    const engineer = new Engineer("Tyler Durden", 4512, "tydurden@gmail.com");
+  it("should contain the engineers's name, id , email and github ", function () {
+    const engineer = new Engineer(
+      "Tyler Durden",
+      4512,
+      "tydurden@gmail.com",
+      "chrischarlesgibson"
+    );
     expect(engineer.name).toEqual("Tyler Durden");
     expect(engineer.id).toEqual(4512);
     expect(engineer.email).toEqual("tydurden@gmail.com");
